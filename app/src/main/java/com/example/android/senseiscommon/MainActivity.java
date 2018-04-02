0e3e26b57cbc30a3d68690d610f845af39627eca
SidP919 <sidp919@gmail.com>
2018-04-03 03:22:14 +0530

    Made many changes,Removed all known errors. If someone finds any please inform.

--- app/src/main/java/com/example/android/senseiscommon/MainActivity.java
+++ app/src/main/java/com/example/android/senseiscommon/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.android.senseiscommon;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +23,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout layout = findViewById(R.id.parent_relative_layout);
+        layout.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent ev)
+            {
+                hideSoftKeyboard(view);
+                return false;
+            }
+        });
     }
+
+    /**
+     * Hides virtual keyboard
+     */
+    protected void hideSoftKeyboard(View view)
+    {
+        InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        in.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
 
     int noOfCorrectAnswers = 0;
     int i = 1;
@@ -47,10 +73,15 @@ public class MainActivity extends AppCompatActivity {
         RadioButton rb = findViewById(R.id.op5);
         return rb;
     }
+    public ImageView getResImg(){
+        ImageView im = findViewById(R.id.result_image);
+        return im;
+    }
     /*
     Asks 1st question and shows submit button
      */
     public void startQuiz(View view){
+        hideSoftKeyboard(view);
         if(NameEntered().isEmpty()){
             Toast.makeText(this, getString(R.string.toast_for_name), Toast.LENGTH_SHORT).show();
         }else{
@@ -76,13 +107,13 @@ public class MainActivity extends AppCompatActivity {
     Submit button changes the question and option on every click and also updates the noOfCorrectAnswers
      */
     public void submitAns(View view){
-        if(i<11){
+        if(i<10){
             switch(i){
                 case 1: {
                     i++;
                     if(getop1().isChecked()){
                         ++noOfCorrectAnswers;
-                        getop1().setChecked(false);
+                        getop1().setTextColor(Color.BLUE);
                     }
                     getQues().setText(R.string.question2);
                     getop1().setText(R.string.q2o1);
@@ -93,10 +124,11 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
                 case 2: {
+
                     i++;
                     if(getop2().isChecked()){
                         ++noOfCorrectAnswers;
-                        getop2().setChecked(false);
+                        getop2().setTextColor(Color.BLUE);
                     }
                     getQues().setText(R.string.question3);
                     getop1().setText(R.string.q3o1);
@@ -111,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
                     i++;
                     if(getop1().isChecked()){
                         ++noOfCorrectAnswers;
-                        getop1().setChecked(false);
+                        getop1().setTextColor(Color.BLUE);
                     }
                     getQues().setText(R.string.question4);
                     getop1().setText(R.string.q4o1);
@@ -125,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
                     i++;
                     if(getop3().isChecked()){
                         ++noOfCorrectAnswers;
-                        getop3().setChecked(false);
+                        getop3().setTextColor(Color.BLUE);
                     }
                     getQues().setText(R.string.question5);
                     getop1().setText(R.string.q5o1);
@@ -141,7 +173,7 @@ public class MainActivity extends AppCompatActivity {
                     i++;
                     if(getop4().isChecked()){
                         ++noOfCorrectAnswers;
-                        getop4().setChecked(false);
+                        getop4().setTextColor(Color.BLUE);
                     }
                     getQues().setText(R.string.question6);
                     getop1().setText(R.string.q6o1);
@@ -155,7 +187,7 @@ public class MainActivity extends AppCompatActivity {
                     i++;
                     if(getop4().isChecked()){
                         ++noOfCorrectAnswers;
-                        getop4().setChecked(false);
+                        getop4().setTextColor(Color.BLUE);
                     }
                     getQues().setText(R.string.question7);
                     getop1().setText(R.string.q7o1);
@@ -169,7 +201,7 @@ public class MainActivity extends AppCompatActivity {
                     i++;
                     if(getop2().isChecked()){
                         ++noOfCorrectAnswers;
-                        getop2().setChecked(false);
+                        getop2().setTextColor(Color.BLUE);
                     }
                     getQues().setText(R.string.question8);
                     getop1().setText(R.string.q8o1);
@@ -183,20 +215,20 @@ public class MainActivity extends AppCompatActivity {
                     i++;
                     if(getop4().isChecked()){
                         ++noOfCorrectAnswers;
-                        getop4().setChecked(false);
+                        getop4().setTextColor(Color.BLUE);
                     }
                     getQues().setText(R.string.question9);
                     getop1().setText(R.string.q9o1);
                     getop2().setText(R.string.q9o2);
                     getop3().setText(R.string.q9o3);
-                    getop4().setText(R.string.q8o4);
+                    getop4().setText(R.string.q9o4);
                 }
                 break;
                 case 9: {
                     i++;
                     if(getop3().isChecked()){
                         ++noOfCorrectAnswers;
-                        getop3().setChecked(false);
+                        getop3().setTextColor(Color.BLUE);
                     }
                     getQues().setText(R.string.question10);
                     getop1().setText(R.string.q10o1);
@@ -207,17 +239,12 @@ public class MainActivity extends AppCompatActivity {
                     getop5().setText(R.string.q10o5);
                 }
                 break;
-                case 10: {
-                    i++;
-                    if(getop4().isChecked()){
-                        ++noOfCorrectAnswers;
-                    }
-                }
-                break;
-
             }
         }
         else{
+            if(getop4().isChecked()){
+                ++noOfCorrectAnswers;
+            }
             LinearLayout quesAns = findViewById(R.id.question_answer);
             quesAns.setVisibility(View.INVISIBLE);
             LinearLayout result = findViewById(R.id.result);
@@ -225,9 +252,11 @@ public class MainActivity extends AppCompatActivity {
             TextView summary = findViewById(R.id.summary);
             if(noOfCorrectAnswers>7 && noOfCorrectAnswers<10){
                 Toast.makeText(this, getString(R.string.if8090Toast) + "You answered " + noOfCorrectAnswers + " correct answers.", Toast.LENGTH_SHORT).show();
+                getResImg().setImageResource(R.drawable.goodjob);
                 summary.setText(R.string.if8090);
             }else if(noOfCorrectAnswers==10){
                 Toast.makeText(this, getString(R.string.if100Toast) + "You answered " + noOfCorrectAnswers + " correct answers.", Toast.LENGTH_SHORT).show();
+                getResImg().setImageResource(R.drawable.respect);
                 summary.setText(R.string.if100);
             }else if(noOfCorrectAnswers<6){
                 Toast.makeText(this, "You answered " + noOfCorrectAnswers + " correct answers.", Toast.LENGTH_SHORT).show();
@@ -236,21 +265,35 @@ public class MainActivity extends AppCompatActivity {
                 summary.setTextColor(Color.RED);
             }else{
                 Toast.makeText(this, getString(R.string.if6070Toast) + "You answered " + noOfCorrectAnswers + " correct answers.", Toast.LENGTH_SHORT).show();
+                getResImg().setImageResource(R.drawable.goodjob);
                 summary.setText(R.string.if6070);
             }
         }
     }
 
-//    public void setGreen(View view){
-//        if(((RadioButton)view).isChecked())
-//            ((RadioButton)view).setTextColor(Color.GREEN);
-//            ((RadioButton)view).setTextColor(Color.BLUE);
-//    }
+    public void checkRB(View view){
+        RadioButton rb = (RadioButton) view;
+        rb.setTextColor(getResources().getColor(R.color.green));
+        if(!rb.equals(getop1())){
+            getop1().setTextColor(Color.BLUE);
+        }
+        if(!rb.equals(getop2())){
+            getop2().setTextColor(Color.BLUE);
+        }
+        if(!rb.equals(getop3())){
+            getop3().setTextColor(Color.BLUE);
+        }
+        if(!rb.equals(getop4())){
+            getop4().setTextColor(Color.BLUE);
+        }
+        if(!rb.equals(getop5())){
+            getop5().setTextColor(Color.BLUE);
+        }
+    }
 
     public void reset(View view){
         i = 1;
         noOfCorrectAnswers = 0;
-
         Button startButton = findViewById(R.id.start_button);
         startButton.setVisibility(View.VISIBLE);
         LinearLayout result = findViewById(R.id.result);
